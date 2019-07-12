22ace586fd99732d36f81181fc19af1eaa72a574
joekennerly <joseph.kennerly@gmail.com>
2019-07-12 12:37:57 -0500

    init

---
+++ src/components/CandyTypes/CandyTypes.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class CandyList extends Component {
+  render() {
+    return (
+      <section className="candyTypes">
+        {this.props.candyTypes.map(type => (
+          <div key={type.id}>{type.name}</div>
+        ))}
+      </section>
+    );
+  }
+}
