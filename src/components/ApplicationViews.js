078082674be3a5b488be36fc4143e1d0d0db8c09
joekennerly <joseph.kennerly@gmail.com>
2019-07-19 11:09:36 -0500

    modify applicationviews

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import EmployeeList from './Employees/Employees';
-import StoreList from './StoreLocations/StoreLocations';
-import CandyTypes from './CandyTypes/CandyTypes';
-import { Route } from 'react-router-dom'
+import React, { Component } from "react";
+import EmployeeList from "./Employees/Employees";
+import StoreList from "./StoreLocations/StoreLocations";
+import CandyTypes from "./CandyTypes/CandyTypes";
+import { Route } from "react-router-dom";
 
 export default class ApplicationViews extends Component {
   state = {
@@ -10,38 +10,43 @@ export default class ApplicationViews extends Component {
     employees: [],
     candyTypes: [],
     candies: []
-  }
+  };
 
   componentDidMount() {
-    const newState = {}
+    const newState = {};
 
     fetch("http://localhost:5002/stores")
-        .then(r => r.json())
-        .then(stores => newState.stores = stores)
-        .then(() => fetch("http://localhost:5002/employees")
-        .then(r => r.json()))
-        .then(employees => newState.employees = employees)
-        .then(() => fetch("http://localhost:5002/candyTypes")
-        .then(r => r.json()))
-        .then(candyTypes => newState.candyTypes = candyTypes)
-        .then(() => fetch("http://localhost:5002/candies")
-        .then(r => r.json()))
-        .then(candies => newState.candies = candies)
-        .then(() => this.setState(newState))
+      .then(r => r.json())
+      .then(stores => (newState.stores = stores))
+      .then(() => fetch("http://localhost:5002/employees").then(r => r.json()))
+      .then(employees => (newState.employees = employees))
+      .then(() => fetch("http://localhost:5002/candyTypes").then(r => r.json()))
+      .then(candyTypes => (newState.candyTypes = candyTypes))
+      .then(() => this.setState(newState));
   }
 
   render() {
     return (
       <React.Fragment>
-        <Route exact path="/" render={(props) => {
-          return <StoreList stores={this.state.stores} />
-        }} />
-        <Route path="/candyTypes" render={(props) => {
-          return <CandyTypes candyTypes={this.state.candyTypes} />
-        }} />
-        <Route path="/employees" render={(props) => {
-          return <EmployeeList employees={this.state.employees} />
-        }} />
+        <Route
+          exact
+          path="/"
+          render={props => {
+            return <StoreList stores={this.state.stores} />;
+          }}
+        />
+        <Route
+          path="/candyTypes"
+          render={props => {
+            return <CandyTypes candyTypes={this.state.candyTypes} />;
+          }}
+        />
+        <Route
+          path="/employees"
+          render={props => {
+            return <EmployeeList employees={this.state.employees} />;
+          }}
+        />
       </React.Fragment>
     );
   }
